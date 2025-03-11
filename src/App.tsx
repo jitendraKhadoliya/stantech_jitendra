d9b0295caa7815ed8761448465af8d09bf933741
Jitendra Khadoliya <jitendrasaini352jjnu@gmail.com>
2025-03-11 23:59:52 +0530

    lazy loading create

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Details from "./pages/Details";
 import { Provider } from "react-redux";
 import store from "./store/store";
+
+// Lazy loading components
+const Home = lazy(() => import("./pages/Home"));
+const Details = lazy(() => import("./pages/Details"));
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/details/:id" element={<Details />} />
-        </Routes>
+        <Suspense fallback={<div className="text-center mt-4">Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/details/:id" element={<Details />} />
+          </Routes>
+        </Suspense>
       </Router>
     </Provider>
   );
